5e3646691dea98e4ad023e93bf766f60528943b9
kaleb-himes <kaleb@wolfssl.com>
2015-01-26 18:29:38 -0700

    starting work on main and identifier_FSA

--- Compiler_Awesome/src/c_a/C_A.java
+++ Compiler_Awesome/src/c_a/C_A.java
@@ -1,21 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package c_a;
 
+import java.util.Scanner;
+
 /**
- *
- * @author khimes
+ * @title c_a = compiler_awesome
+ * @version 0.1
+ * @author monica, tabetha, kaleb
+ * @team ∀wesome
  */
 public class C_A {
+    /* The file location and names example:
+     * 
+     * computer/somewhere/file.java
+     *
+     * fLocation = computer/somewhere/
+     * fName = file.java
+     */
+
+    public static String fLocation = "";
+    public static String fName = "";
 
     /**
-     * @param args the command line arguments
+     * @param args the command line arguments if any
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Example: C:/somedirectory/file.xxx");
+        System.out.println("file Location = C:/somedirectory/");
+        System.out.println("file Name = file.xxx\n");
+        System.out.println("Enter the file Location: ");
+        fLocation = reader.nextLine();
+        System.out.println("Enter the file Name: ");
+        fName = reader.nextLine();
+        identifier_FSA ident = new identifier_FSA();
     }
-    
+
 }
--- Compiler_Awesome/src/c_a/identifier_FSA.java
+++ Compiler_Awesome/src/c_a/identifier_FSA.java
@@ -5,10 +5,29 @@
  */
 package c_a;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
 /**
  *
  * @author khimes
  */
-class identifier_FSA {
-    
+class identifier_FSA extends C_A {
+
+    private static void readFile() throws FileNotFoundException, IOException {
+        BufferedReader reader = new BufferedReader(
+                                new InputStreamReader(
+                                new FileInputStream(fName),
+                                Charset.forName("UTF-8")));
+        int c;
+        while ((c = reader.read()) != -1) {
+            char character = (char) c;
+            System.out.println(character);
+            // Do something with your character
+        }
+    }
 }
