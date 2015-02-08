ccbb5331f4748f39bb39688f311498c3478b5fc0
Monica Thornton <monicasuecooper@hotmail.com>
2015-02-07 22:48:00 -0700

    updated symbol_FSA, is now working for all cases and has been tested

--- Compiler_Awesome/src/c_a/symbol_FSA.java
+++ Compiler_Awesome/src/c_a/symbol_FSA.java
@@ -1,49 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package c_a;
 
-import static c_a.C_A.fLocation;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
+//import static c_a.C_A.fLocation;
+//import java.io.BufferedReader;
+//import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PushbackReader;
-import java.nio.charset.Charset;
+import java.util.Arrays;
+//import java.io.InputStreamReader;
+//import java.io.PushbackReader;
+//import java.nio.charset.Charset;
 
 /**
  * @title c_a = compiler_awesome
  * @version 0.1
  * @author monica, tabetha, kaleb
  * @team ∀wesome
+ *
+ * A finite state automaton, that is responsible for reading in symbols. The FSA
+ * returns the longest lexeme that matches any of the tokens, as well as the
+ * column and line number, and the appropriate token.
+ *
  */
 public class symbol_FSA extends C_A {
+    /* Initializes the State variable to the START state */
 
+    State state = State.START;
+
+    //Strings corresponding to the lexeme (i.e. "+") and token(i.e. MP_PLUS)
     String lexeme = "";
     String token = "";
     char character;
-    String[] chars = {":=", "<=", ">=", ":", ",", "=", "/", ">", "<", "(", "-", ".", "+", ")", ";", "*"};
 
-    /* 
-     * flags to indicate whether or not a particular character
-     * has already been scanned
-     */
-    Boolean readPeriod = false;
-    Boolean readOperator = false;
+    //The string containing building blocks of all valid symbols
+    String[] validSymbols = new String[]{":", ",", "=", "/", ">", "<", "(", "-", ".", "+", ")", ";", "*"};
 
     public enum State {
 
-        START, IDACCEPT, S0
+        START, SINGLEACCEPT, GEQACCEPT, ASSIGNACCEPT, LEQACCEPT, NEQACCEPT
     }
 
-    /* Initializes the State variable to the START state */
-    State state = State.START;
-
+    //Precondition: the source file file pointer points to the first character 
+    //of the lexeme corresponding to the next token
     public void readFile() throws FileNotFoundException, IOException {
-
         int c;
 
         while ((c = MPscanner.pbr.read()) != -1) {
@@ -63,145 +61,107 @@ public class symbol_FSA extends C_A {
                      */
                     character = (char) MPscanner.pbr.read();
 
-                    /* puts the character in the lexeme */
-                    lexeme = Character.toString(character);
+                    //Checks to see if symbols stored in character is a valid
+                    //symbol
+                    if (Arrays.asList(validSymbols).contains(Character.toString(character))) {
+                        // Puts the character in the lexeme 
+                        lexeme = Character.toString(character);
 
-                    /* 
-                     * transitions to IDACCEPT state 
-                     * (because a letter or underscore has been read)
-                     */
-                    state = State.IDACCEPT;
-
+                        //Moves to an accept state, as you have read at 
+                        //least one valid symbol
+                        state = State.SINGLEACCEPT;
+                    }
                     /* end of START case */
                     break;
 
-                /* Accept State for an Identifier Value */
-                case IDACCEPT:
-                    /* read the next character */
-                    character = (char) MPscanner.pbr.read();
-
-                    for (int i = 0; i < chars.length; i++) {
-                        
-                        // if the character is in the array of characters above do this
-                        if (Character.toString(character) == chars[i]) {
-                            if (character == ':') {
-                                //read the next character
-                                character = (char) MPscanner.pbr.read();
-
-                                //check to see if the next character is =
-                                if (character == '=') {
-                                    token = "MP_ASSIGN";
-                                } else {
-                                    token = "MP_COLON";
-                                }
-                            } else if (character == ',') {
-                                token = "MP_COMMA";
-                            } else if (character == '=') {
-                                token = "MP_EQUAL";
-                            } else if (character == '/') {
-                                token = "MP_FLOAT_DIVIDE";
-                            } else if (character == '>') {
-                                //read the next character
-                                character = (char) MPscanner.pbr.read();
-
-                                //check to see if the next character is =
-                                if (character == '=') {
-                                    token = "MP_GEQUAL";
-                                } else {
-                                    token = "MP_GTHAN";
-                                }
-                            } else if (character == '<') {
-                                //read the next character
-                                character = (char) MPscanner.pbr.read();
-
-                                //check to see if the next character is =
-                                if (character == '=') {
-                                    token = "MP_LEQUAL";
-                                //check to see if the next character is >
-                                } else if (character == '>') {
-                                    token = "MP_NEQUAL";
-                                } else {
-                                    token = "MP_LTHAN";
-                                }
-                            } else if (character == '(') {
-                                token = "MP_LPAREN";
-                            } else if (character == '-') {
-                                token = "MP_MINUS";
-                            } else if (character == '.') {
-                                token = "MP_PERIOD";
-                            } else if (character == '+') { 
-                                token = "MP_PLUS";
-                            } else if (character == ')') { 
-                                token = "MP_RPAREN";
-                            } else if (character == ';') { 
-                                token = "MP_SCOLON";
-                            } else { 
-                                token = "MP_TIMES";
-                            }
-                            /* if 0-9 | a-z | A-Z | $ | _ then concat to lexeme */
-                            lexeme = lexeme.concat(Character.toString(character));
-                        } else if (!Character.isAlphabetic(character)
-                                && !Character.isDigit(character)
-                                && character != '_') {
-                            /*
-                             * Checks if character is anything but acceptable
-                             * Identifier value and ensures it has not been read
-                             * previously
-                             */
-                            if (readPeriod == false) {
-                                MPscanner.pbr.unread(character);
-                                state = State.S0;
-                            } else {
-                                /* a bad value has already been read */
-                                MPscanner.pbr.unread(1);
-
-                                token = "MP_IDENTIFIER";
-
-                                /* test print-outs */
-                                System.out.println(state);
-                                System.out.println(lexeme);
-                                System.out.println(token);
-
-                                /* test print-outs */
-                                System.out.println("----------------");
-                                character = (char) MPscanner.pbr.read();
-                                System.out.println(character);
-
-                                /* need to return to dispatcher here but for now exit */
-                                System.exit(0);
-                            }
-                        } else {
-                            /* invalid next character, reset */
-                            MPscanner.pbr.unread(character);
-
-                            token = "MP_IDENTIFIER";
-
-                            /* test print-outs */
-                            System.out.println(state);
-                            System.out.println(lexeme);
-                            System.out.println(token);
-
-                            /* test print-outs */
+                /* Accept State for a valid Symbol Value of length 1*/
+                case SINGLEACCEPT:
+                    //Accept all valid single character tokens                
+                    if (Character.compare(character, ',') == 0) {
+                        token = "MP_COMMA";
+                    } else if (Character.compare(character, '=') == 0) {
+                        token = "MP_EQUAL";
+                    } else if (Character.compare(character, '/') == 0) {
+                        token = "MP_FLOAT_DIVIDE";
+                    } else if (Character.compare(character, '(') == 0) {
+                        token = "MP_LPAREN";
+                    } else if (Character.compare(character, '-') == 0) {
+                        token = "MP_MINUS";
+                    } else if (Character.compare(character, '.') == 0) {
+                        token = token = "MP_PERIOD";
+                    } else if (Character.compare(character, '+') == 0) {
+                        token = token = "MP_PLUS";
+                    } else if (Character.compare(character, ')') == 0) {
+                        token = token = "MP_RPAREN";
+                    } else if (Character.compare(character, ';') == 0) {
+                        token = "MP_SCOLON";
+                    } else if (Character.compare(character, '*') == 0) {
+                        token = "MP_TIMES";
+                        //FSA has handled all single character accept cases
+                    } else {
+                        //FSA may need to handle a multiple character accept case
+                        if (Character.compare(character, '>') == 0) {
+                            //Determine if the next character is =, otherwise accept at >
+                            //and set the reader back
                             character = (char) MPscanner.pbr.read();
-                            System.out.println("--------Reader is at");
-                            System.out.println(Character.toString(character));
-
-                            /* need to return to dispatcher here but for now exit */
-                            System.exit(0);
+                            if (Character.compare(character, '=') == 0) {
+                                lexeme = lexeme.concat(Character.toString(character));
+                                state = State.GEQACCEPT;
+                            } else {
+                                //= was the only valid symbol to follow >, so 
+                                //unread the invalid character
+                                MPscanner.pbr.unread(character);
+                                token = "MP_GTHAN";
+                            }
+                        } else if (Character.compare(character, ':') == 0) {
+                            character = (char) MPscanner.pbr.read();
+                            if (Character.compare(character, '=') == 0) {
+                                lexeme = lexeme.concat(Character.toString(character));
+                                state = State.ASSIGNACCEPT;
+                            } else {
+                                //= was the only valid symbol to follow :, so 
+                                //unread the invalid character                                
+                                MPscanner.pbr.unread(character);
+                                token = "MP_COLON";
+                            }
+                        } else if (Character.compare(character, '<') == 0) {
+                            character = (char) MPscanner.pbr.read();
+                            if (Character.compare(character, '=') == 0) {
+                                lexeme = lexeme.concat(Character.toString(character));
+                                state = State.LEQACCEPT;
+                            } else if (Character.compare(character, '>') == 0) {
+                                lexeme = lexeme.concat(Character.toString(character));
+                                state = State.NEQACCEPT;
+                            } else {
+                                //= and > were the only valid symbols to follow <, so 
+                                //unread the invalid character
+                                MPscanner.pbr.unread(character);
+                                token = "MP_LTHAN";
+                            }
                         }
+                    } //End of if statement that sets token for all valid symbols
 
-                        /* END IDACCEPT */
+                    if (state == State.SINGLEACCEPT) {
+                        /* test print-outs */
+                        System.out.println(state);
+                        System.out.println(lexeme);
+                        System.out.println(token);
+
+                        /* test print-outs */
+                        System.out.println("--------Reader is at");
+                        character = (char) MPscanner.pbr.read();
+                        System.out.println(character);
+                        /* need to return to dispatcher here but for now exit */
+                        System.exit(0);
                     }
+                    // END SINGLEACCEPT
                     break;
 
                 /* 
-                 * S0 state indicates all valid characters have been read and 
-                 * we encountered something not legal. This could mean either
-                 * a valid identifier has been read or an error occured
-                 * let the dispatcher handle it
+                 * GEQACCEPT state indicates that we have read a > and an =.
                  */
-                case S0:
-                    token = "MP_IDENTIFIER";
+                case GEQACCEPT:
+                    token = "MP_GEQUAL";
 
                     /* test print-outs */
                     System.out.println(state);
@@ -209,13 +169,69 @@ public class symbol_FSA extends C_A {
                     System.out.println(token);
 
                     /* test print-outs */
-                    character = (char) MPscanner.pbr.read();
                     System.out.println("--------Reader is at");
-                    System.out.println(Character.toString(character));
-                    MPscanner.pbr.unread(character);
+                    character = (char) MPscanner.pbr.read();
+                    System.out.println(character);
                     /* need to return to dispatcher here but for now exit */
                     System.exit(0);
+                    // END GEQACCEPT
+                    break;
+
+                case LEQACCEPT:
+                    token = "MP_LEQUAL";
+
+                    /* test print-outs */
+                    System.out.println(state);
+                    System.out.println(lexeme);
+                    System.out.println(token);
+
+                    /* test print-outs */
+                    System.out.println("--------Reader is at");
+                    character = (char) MPscanner.pbr.read();
+                    System.out.println(character);
+                    /* need to return to dispatcher here but for now exit */
+                    System.exit(0);
+                    // END LEQACCEPT
+                    break;
+
+                case ASSIGNACCEPT:
+                    token = "MP_ASSIGN";
+
+                    /* test print-outs */
+                    System.out.println(state);
+                    System.out.println(lexeme);
+                    System.out.println(token);
+
+                    /* test print-outs */
+                    System.out.println("--------Reader is at");
+                    character = (char) MPscanner.pbr.read();
+                    System.out.println(character);
+                    /* need to return to dispatcher here but for now exit */
+                    System.exit(0);
+
+                    // END ASSIGNACCEPT
+                    break;
+
+                case NEQACCEPT:
+                    token = "MP_NEQUAL";
+
+                    /* test print-outs */
+                    System.out.println(state);
+                    System.out.println(lexeme);
+                    System.out.println(token);
+
+                    /* test print-outs */
+                    System.out.println("--------Reader is at");
+                    character = (char) MPscanner.pbr.read();
+                    System.out.println(character);
+                    /* need to return to dispatcher here but for now exit */
+                    System.exit(0);
+
+                    // END NEQACCEPT
+                    break;
             }
+            //Post Condition: The input file pointer is pointing at the first 
+            //character after the current token  
         }
     }
 }
