55ba60eb4ac872f5f51cbdfdf1d53591c47302d3
Monica Thornton <monicasuecooper@hotmail.com>
2015-04-18 12:52:00 -0600

    added comments for symbol table

--- Compiler_Awesome/src/symbol_tables/s_table.java
+++ Compiler_Awesome/src/symbol_tables/s_table.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Builds symbol tables, with terminals from the parser and rules provided 
+ * by the grammar. 
+ *
  */
 package symbol_tables;
 
